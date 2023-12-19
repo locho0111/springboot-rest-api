19b1553ed5b3863a22767474a0af368f23b2bc01
Rick <rick.ho@ibm.com>
2023-12-19 12:56:38 -0600

    Changed HelloWorld Path

--- src/main/java/com/rick/springbootrestapi/controller/HelloWorldController.java
+++ src/main/java/com/rick/springbootrestapi/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-  @GetMapping("/hello")
+  @GetMapping("/helloWorld")
   public String HelloWorld() {
     return "Hello World!";
   }
